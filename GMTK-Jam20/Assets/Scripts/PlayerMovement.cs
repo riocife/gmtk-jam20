2e3baf2dce097c2c532658f1f6835c8da1a2d926
gustmes <gustmes@hotmail.com>
2020-07-10 19:44:32 -0400

    Added player move, animation, tree forest pack

---
+++ GMTK-Jam20/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    Vector2 moveInput;
+    Rigidbody2D rb;
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("HorizontalMove", moveInput.x);
+        animator.SetFloat("VerticalMove", moveInput.y);
+        animator.SetFloat("Speed", moveInput.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 targetPos = rb.position + moveInput * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(targetPos);
+    }
+}
