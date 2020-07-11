7925b9ba694e7cab426b0c10eaa3b4d5003a1105
athos santos <athos1rodrigues@gmail.com>
2020-07-11 14:23:21 -0300

    boring

---
+++ GMTK-Jam20/Assets/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, new Vector3(1, 1, 1));
+    }
+}
