ab61b9b75b2d2a00388700ba8f0ceedd466f8564
gustmes <gustmes@hotmail.com>
2020-07-11 17:16:28 -0400

    Adding audio clips

--- GMTK-Jam20/Assets/Scripts/EnemyHealth.cs
+++ GMTK-Jam20/Assets/Scripts/EnemyHealth.cs
@@ -7,17 +7,49 @@ public class EnemyHealth : MonoBehaviour
 {
     public Action onEnemyDied;
 
+    public AudioClip deathClip;
+    AudioSource audioSource;
+
     // Hits needed to die
-    public int hits = 2;
+    public int hits = 1;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            Debug.LogError("Could not find audio source!");
+        }
+    }
 
     public void TakeDamage(int damage)
     {
         hits -= damage;
         if (hits <= 0)
         {
-            onEnemyDied.Invoke();
-            gameObject.SetActive(false);
-            Destroy(gameObject, 0.5f);
+            StartCoroutine(Die());
         }
+    }
+
+    IEnumerator Die()
+    {
+        onEnemyDied?.Invoke();
+
+        audioSource.Play();
+
+        // Stop current clip and change it to explosion
+        //audioSource.Stop();
+        //audioSource.clip = deathClip;
+        //audioSource.loop = false;
+        //audioSource.volume = 1f;
+        //        audioSource.Play();
+
+//        yield return new WaitForSeconds(audioSource.clip.length + 0.1f);
+
+        gameObject.SetActive(false);
+
+        Destroy(gameObject, 0.5f);
+
+        yield return null;
     }
 }
